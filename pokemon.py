e3cb7307a2cc1b6ae3b899a3acb135f5ba119680
Love <babyfaceninja1337@gmail.com>
2017-03-30 23:16:18 +0200

    implemented text data into pokemon class

--- pokemon.py
+++ pokemon.py
@@ -1,27 +1,57 @@
 class Pokemon:
     def __init__(self, pokeId = 0):
+        pokemonlista = open('pokemonlista.txt',"r")
+        line = pokemonlista.readlines()[pokeId].split()
+        pokemonlista.close
+
+        attributes = line[4].split("/")
+        
         self.id = pokeId
-        self.name = 'Pikachu'
+        self.name = line[3]
         self.lvl = 1
-        self.exp = 0
-        self.atk = 10
-        self.hp = 10
-        self.defence = 5
-        self.attribute1 = 'Lightning'
-        self.attribute2 = ''
+        self.experience = 0
+        self.attack = line[6]
+        self.hp = line[5]
+        self.defence = line[7]
+        self.attribute1 = attributes[0]
+        self.spAttack = line[8]
+        self.spDefence = line[9]
+        self.speed = line[10]
+        
+        if len(attributes) > 1:
+            self.attribute2 = attributes[1]
+        else:
+            self.attribute2 = ''
+        
     def lvlup(self):
         self.lvl += 1
+            
+    def getInfo(self):
+        i = 3 - len(str(self.id))
+        pokeId = ''
+        while i > 0:
+            pokeId += '0'
+            i -= 1
+        pokeId += str(self.id)
+        return '#' + pokeId + ', ' + self.name + ' Attributes: ' + self.attribute1 + '/' + self.attribute2 + ', Level: ' + str(self.lvl) + ', Attack: ' + str(self.attack) + ', Defence: ' + str(self.defence)
+from random import randint
 
-    def showLvl(self):
-        print(self.lvl)
 
-Pikachu = Pokemon(15)
 
-Pikachu.showLvl()
 
-Pikachu.lvlup()
+    
 
-Pikachu.showLvl()
+pokemon = Pokemon(randint(1,151))
+
+
+
+pokemon.lvlup()
+
+print(pokemon.getInfo())
+
+
+        
+
     
     
     
